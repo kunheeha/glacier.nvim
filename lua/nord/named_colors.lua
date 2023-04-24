52b236b1d7c89293c9ab4f2d8865bf80a3ba0974
Kunhee Ha <kunheeha@gmail.com>
2023-04-24 20:01:29 +0100

    change palette

--- lua/nord/named_colors.lua
+++ lua/nord/named_colors.lua
@@ -1,5 +1,4 @@
 local nord = {
-	--16 colors
 	black = "#2E3440", -- nord0 in palette
 	dark_gray = "#3B4252", -- nord1 in palette
 	gray = "#434C5E", -- nord2 in palette
@@ -8,15 +7,15 @@ local nord = {
 	darkest_white = "#D8DEE9", -- nord4 in palette
 	darker_white = "#E5E9F0", -- nord5 in palette
 	white = "#ECEFF4", -- nord6 in palette
-	teal = "#5DE4C7", -- nord7 in palette
-	off_blue = "#89DDFF", -- nord8 in palette
-	glacier = "#ADD7FF", -- nord9 in palette
-	blue = "#91B4D5", -- nord10 in palette
+	teal = "#89DDFF", -- nord7 in palette
+	off_blue = "#ADD7FF", -- nord8 in palette
+	glacier = "#91B4D5", -- nord9 in palette
+	blue = "#7390AA", -- nord10 in palette
 	red = "#D0679D", -- nord11 in palette
-	orange = "#FAE4FC", -- nord12 in palette
+	orange = "#FAB387", -- nord12 in palette
 	yellow = "#FFFAC2", -- nord13 in palette
-	green = "#5FB3A1", -- nord14 in palette
-	purple = "#FCC5E9", -- nord15 in palette
+	green = "#5DE4C7", -- nord14 in palette
+	purple = "#CBA6F7", -- nord15 in palette
 	none = "NONE",
 }
 return nord
